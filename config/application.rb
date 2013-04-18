26c3c60b47b871fa2d6efb7cf9237bb09232a1a4
Alex Dolgov <a.n.dolgov@gmail.com>
2013-04-18 16:37:08 +0400

    upd

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,6 @@ module Authproviders
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
@@ -52,6 +51,7 @@ module Authproviders
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
