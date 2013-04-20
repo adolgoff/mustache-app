5eab90d87001d1f98a98bf83be2bdc6d99791a4b
Alex Dolgov <a.n.dolgov@gmail.com>
2013-04-21 00:49:38 +0200

    upd

---
+++ public/data/data.js
@@ -0,0 +1,13 @@
+{		
+	bookmarks : [
+		{ 
+			header: "Manchester Заголовок",
+			descr: "Manchester описание",
+			icon: "http://lenta.ru/favicon.ico",
+			link: "http://lenta.ru/news/2013/04/20/ask/",
+			author: "Alex Dolgov",
+			time: "Manchester",
+			tags: "city, manchester, lol"
+		 }
+	]
+}
