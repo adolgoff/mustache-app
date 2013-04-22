e8217f1cbb19bfcd016d6ef638abd9f0f392d48e
Alex Dolgov <a.n.dolgov@gmail.com>
2013-04-22 14:09:11 +0400

    added some graphics

--- public/js/modules/module-search-box.js
+++ public/js/modules/module-search-box.js
@@ -1,21 +1,37 @@
 Core.createModule("search-box", function(sb) {
-    var input, button, reset;
+    var input, button, reset, quitSearch;
+
+	function addEscapeListener(e){
+		sb.addEvent(document, "keyup", onKeyUp);
+	}
+	
+	function removeEscapeListener(e){
+		sb.removeEvent(document, "keyup", onKeyUp);
+	}
+
+	function onKeyUp(e){
+		if (e.keyCode == 27) quitSearch();
+	}
 
     return {
         init : function () {
             input = sb.find("#search_input")[0],
             button = sb.find("#btn-search")[0],
             reset  = sb.find("#btn-quit")[0];
-            
+			quitSearch = this.quitSearch;            
+			
             sb.addEvent(button, "click", this.handleSearch);
             sb.addEvent(reset, "click", this.quitSearch);
-            console.log(button);
+            sb.addEvent(input, "focus", addEscapeListener);
+            sb.addEvent(input, "focusout", removeEscapeListener);
         },
         
         destroy : function () {
             sb.removeEvent(button, "click", this.handleSearch);
             sb.removeEvent(reset, "click", this.quitSearch);
-            input = button = reset = null;
+            sb.removeEvent(input, "focus", addEscapeListener);
+            sb.removeEvent(input, "focusout", removeEscapeListener);
+            quitSearch = input = button = reset = null;
         },
         
         handleSearch : function (e) {
@@ -29,7 +45,7 @@ Core.createModule("search-box", function(sb) {
             }
         },
         quitSearch : function (e) {
-        	e.preventDefault();
+        	if (e) e.preventDefault();
         	history.pushState ? history.pushState("", document.title, window.location.pathname + window.location.search) : loc.hash = "";
             input.value = "";
             sb.notify({
